a360cba7684115ac990efe25bc9d53b9ab6034a0
codeact1 <mrs.lachakarobinson@gmail.com>
2022-11-22 21:42:45 -0600

    first commit

---
+++ posts/views.py
@@ -0,0 +1,44 @@
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from .models import Post
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+
+
+class PostListView(ListView):
+    template_name = "posts/list.html"
+    model = Post
+
+class PostDetailView(DetailView):
+    template_name = "posts/detail.html"
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    template_name = "posts/new.html"
+    model = Post
+    fields = ['title', 'subtitle','body']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    template_name = "posts/edit.html"
+    model = Post
+    fields = ['title', 'subtitle', 'body']
+
+    def test_func(self):
+        post_obj = self.get_object()
+        return self.request.user == post_obj.author
+
+    
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    template_name = "posts/delete.html"
+    model = Post
+    success_url = reverse_lazy('list')  
+
+    def test_func(self):
+        post_obj = self.get_object()
+        return self.request.user == post_obj.author
